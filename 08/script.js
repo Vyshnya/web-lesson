482cd9b35871aa2efb39e39fb683a5284eac42a5
Vyshnya <vyshnevsky.dmytro@kmrf.kiev.ua>
2022-04-12 13:55:33 +0300

    12.04.2022

---
+++ 08/script.js
@@ -0,0 +1,21 @@
+function task_1() {
+    document.write('Доброго вечора, ми з України!!!!!');
+}
+
+function task_2() {
+    document.write('<i>Паляниця</i>');
+}
+
+function task_3() {
+    let str = 'Все буде Україна!';
+    document.write('<i>' + str + '</i>');
+}
+
+function task_4() {
+    let number = 1;
+    while(number != 6) {
+        document.write(number + '<br>');
+        number++;
+    }
+    
+}
